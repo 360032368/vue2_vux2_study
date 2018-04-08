1642e1ff8a7fc8a0a4ed06136e54187ad3ab7ac8
zhanglongyufei <360032368qq.com>
2018-04-08 17:29:19 +0800

    first commit

---
+++ src/components/Home.vue
@@ -0,0 +1,35 @@
+<template>
+  <div>
+    <front-slide></front-slide>
+    <front-nav></front-nav>
+    <front-list></front-list>
+    <app-footer></app-footer>
+  </div>
+</template>
+
+<style>
+
+</style>
+
+<script>
+import AppFooter from './Footer'
+import FrontSlide from './FrontSlide'
+import FrontNav from './FrontNav'
+import FrontList from './FrontList'
+
+export default {
+  components: {
+    FrontSlide,
+    FrontNav,
+    FrontList,
+    AppFooter
+  },
+  ready () {},
+  methods: {
+  },
+  data () {
+    return {
+    }
+  }
+}
+</script>
